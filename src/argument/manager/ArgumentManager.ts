6991ceb01c738ee50d8f20592b1ea366d192393d
rib_yaboi <iloveproxies@protonmail.com>
2019-02-28 23:29:33 +0100

    some more work on parser

--- src/argument/manager/ArgumentManager.ts
+++ src/argument/manager/ArgumentManager.ts
@@ -1,13 +1,10 @@
 import {CommandSchema} from "../../command/schema/CommandSchema";
-import {ArgumentSchema, OptionalArgumentSchema} from "../schema/ArgumentSchema";
+import {ArgumentSchema, OptionalArgumentSchema, RequiredArgumentSchema} from "../schema/ArgumentSchema";
 import {Argument} from "../Argument";
+import {NARGS} from "../schema/builder/ArgumentBuilder";
 
 export class CommandParser {
-    argumentParser: ArgumentParser;
-
-    constructor() {
-        this.argumentParser = new ArgumentParser();
-    }
+    private argumentParser: ArgumentParser;
 
     parse(schemas: CommandSchema[], inputArguments: string[]): any {
         const proposedCommandName = inputArguments[0];
@@ -19,121 +16,250 @@ export class CommandParser {
                 const detectedCommandSchema = schemas[i];
                 const remainingMessageContents = inputArguments.splice(1);
 
-                this.argumentParser.parse(detectedCommandSchema, remainingMessageContents);
+                this.argumentParser = new ArgumentParser(detectedCommandSchema.argumentSchema, detectedCommandSchema.optionalArgumentSchema);
+                this.argumentParser.parse(remainingMessageContents);
             }
         }
+        return null
     }
 }
 
 export class ArgumentParser {
-    schemas: ArgumentSchema[];
-    optionalSchemas: OptionalArgumentSchema[];
+    private requiredSchemas: RequiredArgumentSchema[];
+    private optionalSchemas: OptionalArgumentSchema[];
+    private inputArguments: string[];
 
-    parse(commandSchema: CommandSchema, inputArguments: string[]): any {
-        this.schemas = commandSchema.argumentSchema;
-        this.optionalSchemas = commandSchema.optionalArgumentSchema;
+
+    constructor(schemas: RequiredArgumentSchema[], optionalSchemas: OptionalArgumentSchema[]) {
+        this.requiredSchemas = schemas;
+        this.optionalSchemas = optionalSchemas;
+    }
+
+    _inputArguments(inputArguments: string[]): void {
+        this.inputArguments = inputArguments
+    }
+
+    parse(inputArguments: string[]): Argument[] {
+        this.inputArguments = inputArguments;
 
         const parsedArguments: Argument[] = [];
 
-        const optionalArgumentIndexResults = this.getOptionalArgumentsIndexResults(this.optionalSchemas, inputArguments);
+        const optionalArgumentIndexResults = this.getOptionalSchemaIdentifierIndexResults(this.optionalSchemas);
 
-        let currentArgSchema;
+        let currentIndexResult;
+        let currentValue;
 
-        for (let i = 0; i < this.schemas.length; i++) {
-            currentArgSchema = this.schemas[i];
-
-
-            // if (!currentArgSchema.required) {
-            //     //we want to get argument values here
-            //     //we get the number of arguments of values for a schema
-            //     let proposedArgumentValues;
-            //     if (typeof currentArgSchema.numArgs === 'number') {
-            //         identifierIndex = identifierIndex + 1;
-            //         proposedArgumentValues = inputArguments.slice(identifierIndex, identifierIndex + currentArgSchema.numArgs);
-            //         console.log("values", proposedArgumentValues)
-            //     } else if (typeof currentArgSchema.numArgs === 'string') {
-            //         //ambiguous. find a method to deal with enum values
-            //     } else if (optionalArgumentSchema.isFlag) {
-            //         proposedArgumentValues = null;
-            //         console.log("flag found");
-            //     }
-            //
-            // }
+        for (let i = 0; i < optionalArgumentIndexResults.length; i++) {
+            currentIndexResult = optionalArgumentIndexResults[i];
+            currentValue = this.getOptionalValueIndexResult(currentIndexResult, optionalArgumentIndexResults);
+            console.log(this.getOptionalValueForIndexResult(currentValue));
         }
+
         return parsedArguments
     }
 
-    parseOptionalArguments(optionalArgumentSchemas: OptionalArgumentSchema[], inputArguments: string[]): any {
+    getOptionalValueForIndexResult(valueIndexResult: OptionalSchemaValueIndexResult): string[] | boolean {
+        if(valueIndexResult.defaultValue) {
+            return valueIndexResult.defaultValue
+        } else if(valueIndexResult.flagValue) {
+            return valueIndexResult.flagValue
+        } else {
+            if (valueIndexResult.from === valueIndexResult.to) {
+                [this.inputArguments[valueIndexResult.from]]
+            }
+            return this.inputArguments.slice(valueIndexResult.from, valueIndexResult.to);
+        }
+    }
+
+    getOptionalValueIndexResults(indexResults: OptionalSchemaIdentifierIndexResult[]): OptionalSchemaValueIndexResult[] {
+        return indexResults.map(indexResult => this.getOptionalValueIndexResult(indexResult, indexResults))
+    }
+
+    private getOptionalValueIndexResult(indexResult: OptionalSchemaIdentifierIndexResult, otherIndexResults: OptionalSchemaIdentifierIndexResult[]): OptionalSchemaValueIndexResult {
+        if (indexResult.schema.isFlag) {
+            return {
+                from: indexResult.identifierIndex,
+                to: indexResult.identifierIndex,
+                identifierIndexResult: indexResult,
+                flagValue: true
+            }
+        }
+
+        if (typeof indexResult.schema.numArgs === "number") {
+            return this.getValueResultForExplicitNumberOfArgs(indexResult, otherIndexResults);
+        } else {
+            return this.getValueResultForAmbigousNumberOfArgs(indexResult, otherIndexResults);
+        }
+    }
+
+    private getValueResultForExplicitNumberOfArgs(indexResult: OptionalSchemaIdentifierIndexResult, indexResults: OptionalSchemaIdentifierIndexResult[]): OptionalSchemaValueIndexResult {
+        const numArgs = <number>indexResult.schema.numArgs;
+        const nextIndexResult = indexResults[indexResults.indexOf(indexResult) + 1];
+
+        let firstValueIndex = indexResult.identifierIndex + 1;
+        let distance;
+
+        if (nextIndexResult) {
+            distance = nextIndexResult.identifierIndex - firstValueIndex;
+        } else {
+            distance = this.inputArguments.length - firstValueIndex;
+        }
+
+        if (distance < numArgs) {
+            throw new Error(`Expected ${numArgs} arguments but got ${distance}.`);
+        }
+
+        return {
+            identifierIndexResult: indexResult,
+            from: firstValueIndex,
+            to: firstValueIndex + numArgs,
+        }
 
     }
 
-    getOptionalArgumentsIndexResults(optionalArgumentSchemas: OptionalArgumentSchema[], inputArguments: string[]): OptionalSchemaIdentifierIndexResult[] {
+    private getValueResultForAmbigousNumberOfArgs(indexResult: OptionalSchemaIdentifierIndexResult, indexResults: OptionalSchemaIdentifierIndexResult[]): OptionalSchemaValueIndexResult {
+        const numArgs = <string>indexResult.schema.numArgs;
+
+        let to;
+
+        const nextIndexResult = indexResults[indexResults.indexOf(indexResult) + 1];
+        const firstValueIndex = indexResult.identifierIndex + 1;
+
+        if (nextIndexResult) {
+            to = nextIndexResult.identifierIndex;
+        } else {
+            to = this.inputArguments.length - firstValueIndex
+        }
+
+        if (numArgs === NARGS.ALL_OR_DEFAULT) {
+            if (firstValueIndex - to === 0) {
+                let defaultValue;
+
+                if(indexResult.schema.default instanceof Array) {
+                    defaultValue = indexResult.schema.default
+                } else if(indexResult.schema.default) {
+                    defaultValue = [indexResult.schema.default.toString()]
+                } else {
+                    throw new Error("Got 0 arguments and no default value.")
+                }
+
+                return {
+                    identifierIndexResult: indexResult,
+                    from: firstValueIndex,
+                    to: to,
+                    defaultValue: defaultValue
+                }
+            }
+        } else if (numArgs === NARGS.ALL_OR_ONE) {
+            if (firstValueIndex - to < 1) {
+                throw new Error("Expected at least 1 argument but got 0.");
+            }
+        }
+
+        return {
+            from: firstValueIndex,
+            to: to,
+            identifierIndexResult: indexResult
+        }
+    }
+
+    getOptionalSchemaIdentifierIndexResults(optionalArgumentSchemas: OptionalArgumentSchema[]): OptionalSchemaIdentifierIndexResult[] {
         return optionalArgumentSchemas
-            .map(optionalArgumentSchema => this.getIndexResultFor(optionalArgumentSchema, inputArguments))
-            .filter(optional => optional.isPresent())
-            .map(optional => optional.getItem());
+            .map(optionalArgumentSchema => this.getIndexResultFor(optionalArgumentSchema))
+            .filter(isPresent)
+            .map(getOptional)
+            .sort((indexResult, anotherIndexResult) => indexResult.index > anotherIndexResult.index ? 1 : -1)
     }
 
-    private getIndexResultFor(optionalArgumentSchema: OptionalArgumentSchema, inputArguments: string[]): Optional<OptionalSchemaIdentifierIndexResult> {
-        const identifierIndexResults = this.getIndexResultsForIdentifiers(optionalArgumentSchema.identifiers, inputArguments);
+    private getIndexResultFor(optionalArgumentSchema: OptionalArgumentSchema): Optional<OptionalSchemaIdentifierIndexResult> {
+        const identifierIndexResults = this.getIndexResultsForIdentifiers(optionalArgumentSchema.identifiers);
         let firstDetectedIndexResult = identifierIndexResults[0];
 
         if (identifierIndexResults.length === 0) {
-            return new Optional<OptionalSchemaIdentifierIndexResult>(null);
+            return Optional.empty()
         } else if (identifierIndexResults.length > 1) {
-            const allIndexes = identifierIndexResults.map(result => result.index);
+            const allIndexes = identifierIndexResults.map(result => result.identifierIndex);
             const lowestIndex = Math.min(...allIndexes);
             firstDetectedIndexResult = identifierIndexResults[allIndexes.indexOf(lowestIndex)];
         }
-        return new Optional({
+        return Optional.of({
             schema: optionalArgumentSchema,
-            index: firstDetectedIndexResult.index,
+            identifierIndex: firstDetectedIndexResult.identifierIndex,
             usedIdentifier: firstDetectedIndexResult.usedIdentifier
         })
     }
 
-    private getIndexResultsForIdentifiers(identifiers: string[], inputArguments: string[]): IdentifierIndexResult[] {
+    private getIndexResultsForIdentifiers(identifiers: string[]): IdentifierIndexResult[] {
         return identifiers
-            .map(identifier => this.getIndexResultForIdentifier(identifier, inputArguments))
-            .filter(optional => optional.isPresent())
-            .map(optional => optional.getItem())
+            .map(identifier => this.getIndexResultForIdentifier(identifier))
+            .filter(isPresent)
+            .map(getOptional)
     }
 
-    private getIndexResultForIdentifier(identifier: string, inputArguments: string[]): Optional<IdentifierIndexResult> {
-        let identifierIndex = inputArguments.indexOf(identifier);
-        let lastIdentifierIndex = inputArguments.lastIndexOf(identifier);
+    private getIndexResultForIdentifier(identifier: string): Optional<IdentifierIndexResult> {
+        let identifierIndex = this.inputArguments.indexOf(identifier);
+        let lastIdentifierIndex = this.inputArguments.lastIndexOf(identifier);
 
         let result = null;
 
         if (identifierIndex >= 0 && lastIdentifierIndex === identifierIndex) {
             result = {
-                index: identifierIndex,
+                identifierIndex: identifierIndex,
                 usedIdentifier: identifier
             }
         }
-        return new Optional(result);
+        return Optional.of(result);
     }
+}
 
-    getEstimationOfNumberOfArguments(): number {
-        return 1
-    }
+function isPresent(optional: Optional<any>) {
+    return optional.isPresent();
+}
+
+function getOptional(optional: Optional<any>) {
+    return optional.getItem();
+}
+
+export interface RequiredSchemaValueIndexResult {
+    readonly from: number,
+    readonly to: number,
+    readonly exceptions?: number[]
+}
+
+export interface OptionalSchemaValueIndexResult {
+    readonly identifierIndexResult: OptionalSchemaIdentifierIndexResult
+    readonly from: number,
+    readonly to: number,
+    readonly defaultValue?: any,
+    readonly flagValue?: boolean
 }
 
 export interface OptionalSchemaIdentifierIndexResult {
-    schema: OptionalArgumentSchema,
-    index: number,
-    usedIdentifier: string
+    readonly schema: OptionalArgumentSchema,
+    readonly identifierIndex: number,
+    readonly usedIdentifier: string
 }
 
 export interface IdentifierIndexResult {
-    index: number,
-    usedIdentifier: string
+    readonly identifierIndex: number,
+    readonly usedIdentifier: string
 }
 
 export class Optional<E> {
-    item: E | null;
+    private readonly item: E;
 
-    constructor(item: E | null) {
+    static of<E>(value: E | null): Optional<E> {
+        if (value === null) {
+            return Optional.empty()
+        }
+        return new Optional<E>(value);
+    }
+
+    static empty(): Optional<any> {
+        return new Optional(null);
+    }
+
+    constructor(item: E) {
         this.item = item;
     }
 
@@ -142,9 +268,6 @@ export class Optional<E> {
     }
 
     getItem(): E {
-        if (this.isPresent()) {
-            return <E>this.item;
-        }
-        throw new TypeError("item is null.")
+        return this.item;
     }
 }
--- test/Parser.test.ts
+++ test/Parser.test.ts
@@ -2,7 +2,7 @@ import * as chai from "chai";
 import {command} from "../src/command/builder/commandBuilder";
 import {argument, optionalArgument} from "../src/argument/schema/builder/argument";
 import {ArgumentParser, CommandParser} from "../src/argument/manager/ArgumentManager";
-import {OptionalArgumentSchema} from "../src/argument/schema/ArgumentSchema";
+import {OptionalArgumentSchema, RequiredArgumentSchema} from "../src/argument/schema/ArgumentSchema";
 
 const expect = chai.expect;
 
@@ -25,7 +25,7 @@ describe("Parser Test", () => {
 
     const testArgumentBuilders = [testArgumentBuilderRequired, testArgumentBuilderOptionalString, testArgumentBuilderOptionalNumber];
 
-    const testArguments = [testArgumentRequired, testArgumentOptionalString, testArgumentOptionalNumber];
+    const testRequiredArguments: RequiredArgumentSchema[] = [testArgumentRequired];
     const testOptionalArguments: OptionalArgumentSchema[] = [testArgumentOptionalString, testArgumentOptionalNumber];
 
     const testCommandBuilder = command("test")
@@ -38,57 +38,116 @@ describe("Parser Test", () => {
     describe("CommandParser Test", () => {
         it("should find the correct command for input", () => {
             const parser = new CommandParser();
-            parser.parse(testCommands, ["!test", "abc", "-n", "15", "-s", "https://google.com", "bad_string"]);
+            parser.parse(testCommands, ["!test", "abc", "-n", "15", "-s", "https://google.com", "bad_string", "test"]);
         });
     });
 
     describe("ArgumentParser Test", () => {
         it("should find the correct argument for input", () => {
-            const parser = new ArgumentParser();
-            parser.parse(testCommands[0], ["abc", "-n", "15", "-s", "https://google.com", "bad_string"])
+            const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
+            parser.parse(["abc", "-n", "15", "adawd", "-s", "https://google.com", "bad_string", "test", "test123"]);
         });
 
         describe("Parsing Optional Arguments", () => {
+            describe("Finding index results", () => {
+                describe("Finding Identifier Index Results", () => {
+                    it("should find index results", () => {
+                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
+                        parser._inputArguments(["abc", "-n", "15"]);
+                        const results = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
 
-            it("should find index results", () => {
-                const parser = new ArgumentParser();
-                const results = parser.getOptionalArgumentsIndexResults(testOptionalArguments, ["abc", "-n",  "15"]);
-                expect(results).to.be.an("array");
-                expect(results.length).to.equal(1);
-                expect(results[0].schema.name).to.equal("number");
-                expect(results[0].index).to.equal(1);
-                expect(results[0].usedIdentifier).to.equal("-n")
+                        expect(results.length).to.equal(1);
+
+                        expect(results[0].schema.name).to.equal("number");
+                        expect(results[0].identifierIndex).to.equal(1);
+                        expect(results[0].usedIdentifier).to.equal("-n")
+                    });
+
+                    it("should find the correct index result for multiple used arguments", () => {
+                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
+                        parser._inputArguments(["abc", "--number", "-n", "15", "-s"]);
+                        const results = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
+
+                        expect(results.length).to.equal(2);
+
+                        expect(results[0].schema.name).to.equal("number");
+                        expect(results[0].identifierIndex).to.equal(1);
+                        expect(results[0].usedIdentifier).to.equal("--number");
+
+                        expect(results[1].schema.name).to.equal("string");
+                        expect(results[1].identifierIndex).to.equal(4);
+                        expect(results[1].usedIdentifier).to.equal("-s");
+                    });
+
+                    it("should only find the index for the first supplied identifier", () => {
+                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
+                        parser._inputArguments(["abc", "-n", "--number", "15"]);
+                        const results = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
+
+                        expect(results.length).to.equal(1);
+
+                        expect(results[0].schema.name).to.equal("number");
+                        expect(results[0].identifierIndex).to.equal(1);
+                        expect(results[0].usedIdentifier).to.equal("-n")
+                    });
+
+                    it("should find no index results if none of the supplied schemas were detected", () => {
+                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
+                        parser._inputArguments(["abc", "def"]);
+                        const results = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
+
+                        expect(results.length).to.equal(0);
+                    })
+                });
+
+                describe("Finding Value Index Results for explicit numberOfArguments", () => {
+                    it("should find index results for a value", () => {
+                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
+                        parser._inputArguments(["--number", "15"]);
+                        const identifierIndexResults = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
+                        const results = parser.getOptionalValueIndexResults(identifierIndexResults);
+
+                        expect(results.length).to.equal(1);
+                        expect(results[0].from).to.equal(1);
+                        expect(results[0].to).to.equal(2);
+                        expect(results[0].identifierIndexResult).to.deep.equal(identifierIndexResults[0]);
+                    })
+                });
+
+                describe("Finding Value Index Results for ambigous numberOfArguments", () => {
+                    const testArgumentBuilderOptionalNumberAmbigousBuilder = optionalArgument("number", ["--number", "-n"])
+                        .numberOfArguments("+")
+                        .number()
+                        .min(10)
+                        .max(20);
+                    const testArgumentBuilderOptionalNumberAmbigous = <OptionalArgumentSchema>testArgumentBuilderOptionalNumberAmbigousBuilder.build();
+
+                    const testArgumentBuilderOptionalStringAmbigousBuilder = optionalArgument("string", ["--string", "-s"])
+                        .numberOfArguments("?")
+                        .default("my string!")
+                        .string()
+                    const testArgumentBuilderOptionalStringAmbigous = <OptionalArgumentSchema>testArgumentBuilderOptionalStringAmbigousBuilder.build();
+
+                    const testOptionalArgumentsAmbigous: OptionalArgumentSchema[] = [testArgumentBuilderOptionalStringAmbigous, testArgumentBuilderOptionalNumberAmbigous];
+
+                    it("should find index results for a value", () => {
+                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArgumentsAmbigous);
+                        parser._inputArguments(["--number", "15"]);
+                        const identifierIndexResults = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArgumentsAmbigous);
+                        const results = parser.getOptionalValueIndexResults(identifierIndexResults);
+
+                        expect(results.length).to.equal(2);
+
+                        expect(results[0].from).to.equal(1);
+                        expect(results[0].to).to.equal(2);
+                        expect(results[0].identifierIndexResult).to.deep.equal(identifierIndexResults[0]);
+
+                        expect(results[1].from).to.equal(1);
+                        expect(results[1].to).to.equal(2);
+                        expect(results[1].identifierIndexResult.schema.name).to.equal(testArgumentBuilderOptionalStringAmbigous.name);
+                    })
+                });
             });
-
-            it("should find the correct index result for multiple used arguments", () => {
-                const parser = new ArgumentParser();
-                const results = parser.getOptionalArgumentsIndexResults(testOptionalArguments, ["abc", "--number", "-n",  "15", "-s"]);
-                expect(results).to.be.an("array");
-                expect(results.length).to.equal(2);
-                expect(results[1].schema.name).to.equal("number");
-                expect(results[1].index).to.equal(1);
-                expect(results[1].usedIdentifier).to.equal("--number");
-                expect(results[0].schema.name).to.equal("string");
-                expect(results[0].index).to.equal(4);
-                expect(results[0].usedIdentifier).to.equal("-s");
-            });
-
-            it("should only find the index for the first supplied identifier", () => {
-                const parser = new ArgumentParser();
-                const results = parser.getOptionalArgumentsIndexResults(testOptionalArguments, ["abc", "-n", "--number", "15"]);
-                expect(results).to.be.an("array");
-                expect(results.length).to.equal(1);
-                expect(results[0].schema.name).to.equal("number");
-                expect(results[0].index).to.equal(1);
-                expect(results[0].usedIdentifier).to.equal("-n")
-            });
-
-            it("should find no index results if none of the supplied schemas were detected", () => {
-                const parser = new ArgumentParser();
-                const results = parser.getOptionalArgumentsIndexResults(testOptionalArguments, ["abc", "def"]);
-                expect(results).to.be.an("array");
-                expect(results.length).to.equal(0);
-            })
         })
     });
 });
