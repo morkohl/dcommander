e0fde9c5dbb743c7ce2d6b3cf338b9f4bbf81082
Moritz Kohl <iloveproxies@protonmail.com>
2019-03-02 17:27:37 +0100

    add new parser

---
+++ src/argument/parser/ArgumentParser.ts
@@ -0,0 +1,166 @@
+import {ArgumentSchema, OptionalArgumentSchema, RequiredArgumentSchema} from "../schema/ArgumentSchema";
+import {NARGS} from "../schema/builder/ArgumentBuilder";
+import {Argument} from "../Argument";
+import {OptionalArgumentValueHolder, RequiredArgumentValueHolder} from "./ArgumentValueHolder";
+import {OptionalArgumentValueHolders, RequiredArgumentValueHolders} from "./ArgumentValueHolders";
+
+export class ArgumentParser {
+    protected requiredSchemas: RequiredArgumentSchema[];
+    protected optionalSchemas: OptionalArgumentSchema[];
+    protected inputArguments: string[];
+
+    private currentRequired: RequiredArgumentValueHolder | null;
+    private currentOptional: OptionalArgumentValueHolder | null;
+
+    private outputArguments: Argument[];
+    private requiredValueHolders: RequiredArgumentValueHolders;
+    private optionalValueHolders: OptionalArgumentValueHolders;
+
+    constructor(schemas: RequiredArgumentSchema[], optionalSchemas: OptionalArgumentSchema[]) {
+        this.requiredSchemas = schemas;
+        this.optionalSchemas = optionalSchemas;
+    }
+
+    parse(inputArguments: string[]) {
+        this.initNewParse(inputArguments);
+
+        let inputArgument: string;
+
+        for (let i = 0; i < inputArguments.length; i++) {
+            inputArgument = inputArguments[i];
+
+            if (this.isIdentifier(inputArgument)) {
+                this.handleIfIdentifierToken(inputArgument);
+            } else if (this.currentOptional) {
+                this.handleIfCurrentOptionalSet(inputArgument, this.currentOptional);
+            } else if (this.currentRequired) {
+                this.handleIfCurrentRequiredSet(inputArgument, this.currentRequired);
+            } else {
+                throw new Error("Too many arguments");
+            }
+        }
+
+        console.log(this.requiredValueHolders.argumentSchemaValueHolders);
+        console.log(this.optionalValueHolders.argumentSchemaValueHolders);
+    }
+
+    isIdentifier(token: string): boolean {
+        let currentSchema: OptionalArgumentSchema;
+        let currentIdentifier: string;
+        for (let i = 0; i < this.optionalSchemas.length; i++) {
+            currentSchema = this.optionalSchemas[i];
+            for (let j = 0; j < currentSchema.identifiers.length; j++) {
+                currentIdentifier = currentSchema.identifiers[j];
+                if (currentIdentifier === token) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private initNewParse(inputArguments: string[]) {
+        this.inputArguments = inputArguments;
+
+        this.currentOptional = null;
+        this.currentRequired = null;
+
+        this.outputArguments = [];
+        this.requiredValueHolders = new RequiredArgumentValueHolders();
+        this.optionalValueHolders = new OptionalArgumentValueHolders();
+
+        if (this.requiredSchemas.length !== 0) {
+            this.currentRequired = new RequiredArgumentValueHolder(this.requiredSchemas[0]);
+            this.requiredValueHolders.add(this.currentRequired);
+        }
+    }
+
+    private handleIfIdentifierToken(token: string): void {
+        if (this.currentOptional) {
+            if (this.currentOptional.numberOfArgumentsAreAmbiguous()) {
+                if (this.currentOptional.numberOfArgumentsAreAmbiguousSpecific(NARGS.AT_LEAST_ONE)) {
+                    if (this.currentOptional.values.length < 2) {
+                        throw new Error("Expected at least one argument");
+                    }
+                } else {
+                    this.currentOptional.setFilled();
+                }
+            } else {
+                if (!this.currentOptional.hasAllArgsSupplied()) {
+                    throw new Error(`Expected ${this.currentOptional.schema.numArgs} arguments but got ${this.currentOptional.values.length}`)
+                }
+            }
+        }
+        this.setCurrentIdentifier(token);
+    }
+
+    private setCurrentIdentifier(token: string) {
+        const detectedSchema = this.getOptionalForIdentifier(token);
+        const optionalValueHolder = new OptionalArgumentValueHolder(detectedSchema);
+        this.currentOptional = optionalValueHolder;
+        this.optionalValueHolders.add(optionalValueHolder);
+    }
+
+    private handleIfCurrentOptionalSet(token: string, currentOptional: OptionalArgumentValueHolder) {
+        this.currentOptional = currentOptional;
+
+        if (this.currentOptional.isFull()) {
+            this.currentOptional = null;
+        } else {
+            this.addToCurrentOptional(token)
+        }
+    }
+
+    private handleIfCurrentRequiredSet(token: string, currentRequired: RequiredArgumentValueHolder) {
+        this.currentRequired = currentRequired;
+
+        if (this.currentRequired.isFull()) {
+            this.handleIfCurrentRequiredSetAndFull(token, currentRequired);
+        } else {
+            this.addToCurrentRequired(token);
+        }
+    }
+
+    private handleIfCurrentRequiredSetAndFull(token: string, currentRequired: RequiredArgumentValueHolder) {
+        this.currentRequired = currentRequired;
+
+        const indexOfCurrentRequiredSchema = this.requiredSchemas.indexOf(this.currentRequired.schema);
+        if (indexOfCurrentRequiredSchema + 1 !== this.requiredSchemas.length) {
+            const nextRequiredSchema = this.requiredSchemas[indexOfCurrentRequiredSchema + 1];
+            const requiredValueHolder = new RequiredArgumentValueHolder(nextRequiredSchema);
+            requiredValueHolder.addValue(token);
+            this.currentRequired = requiredValueHolder;
+            this.requiredValueHolders.add(requiredValueHolder);
+        } else {
+            throw new Error("Too many arguments");
+        }
+    }
+
+    private addToCurrentOptional(token: string): void {
+        if (this.currentOptional) {
+            this.currentOptional = this.optionalValueHolders.updateValueHolderValues(this.currentOptional, token)
+        }
+    }
+
+    private addToCurrentRequired(token: string): void {
+        if (this.currentRequired) {
+            this.currentRequired = this.requiredValueHolders.updateValueHolderValues(this.currentRequired, token);
+        }
+    }
+
+    getOptionalForIdentifier(identifier: string): OptionalArgumentSchema {
+        let currentSchema: OptionalArgumentSchema;
+        let currentIdentifier: string;
+        for (let i = 0; i < this.optionalSchemas.length; i++) {
+            currentSchema = this.optionalSchemas[i];
+            for (let j = 0; j < currentSchema.identifiers.length; j++) {
+                currentIdentifier = currentSchema.identifiers[j];
+                if (currentIdentifier === identifier) {
+                    return currentSchema;
+                }
+            }
+        }
+        throw new Error(`Invalid Argument supplied: ${identifier}`);
+    }
+
+}
--- test/Parser.test.ts
+++ test/Parser.test.ts
@@ -1,14 +1,14 @@
-import * as chai from "chai";
-import {command} from "../src/command/builder/commandBuilder";
+import * as chai from 'chai';
 import {argument, optionalArgument} from "../src/argument/schema/builder/argument";
-import {ArgumentParser, CommandParser} from "../src/argument/manager/ArgumentManager";
 import {OptionalArgumentSchema, RequiredArgumentSchema} from "../src/argument/schema/ArgumentSchema";
+import {command} from "../src/command/builder/commandBuilder";
+import {ArgumentParser} from "../src/argument/parser/ArgumentParser";
 
 const expect = chai.expect;
 
-describe("Parser Test", () => {
+describe('Argument Parser Test', () => {
     const testArgumentBuilderRequired = argument("xyz");
-    const testArgumentRequired = argument("xyz").build();
+    const testArgumentRequired = testArgumentBuilderRequired.build();
 
     const testArgumentBuilderOptionalString = optionalArgument("string", ["--string", "-s"])
         .numberOfArguments(3)
@@ -34,120 +34,42 @@ describe("Parser Test", () => {
         .execute(console.log);
 
     const testCommands = [testCommandBuilder.build()];
-
-    describe("CommandParser Test", () => {
-        it("should find the correct command for input", () => {
-            const parser = new CommandParser();
-            parser.parse(testCommands, ["!test", "abc", "-n", "15", "-s", "https://google.com", "bad_string", "test"]);
-        });
-    });
-
-    describe("ArgumentParser Test", () => {
-        it("should find the correct argument for input", () => {
+    describe("Detecting Identifiers", () => {
+        it("should detect an identifier", () => {
             const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
-            parser.parse(["abc", "-n", "15", "adawd", "-s", "https://google.com", "bad_string", "test", "test123"]);
+            const result: boolean = parser.isIdentifier(testArgumentOptionalString.identifiers[0]);
+
+            expect(result).to.be.true;
+
+            const anotherResult: boolean = parser.isIdentifier("not an identifier");
+
+            expect(anotherResult).to.be.false;
         });
 
-        describe("Parsing Optional Arguments", () => {
-            describe("Finding index results", () => {
-                describe("Finding Identifier Index Results", () => {
-                    it("should find index results", () => {
-                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
-                        parser._inputArguments(["abc", "-n", "15"]);
-                        const results = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
+        it("should get the appropriate identifier", () => {
+            const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
+            const result: OptionalArgumentSchema = parser.getOptionalForIdentifier(testArgumentOptionalString.identifiers[0]);
 
-                        expect(results.length).to.equal(1);
+            expect(result).to.deep.equal(testArgumentOptionalString);
+        });
 
-                        expect(results[0].schema.name).to.equal("number");
-                        expect(results[0].identifierIndex).to.equal(1);
-                        expect(results[0].usedIdentifier).to.equal("-n")
-                    });
+        it("should throw an error if no valid identifier was provided", () => {
+            const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
 
-                    it("should find the correct index result for multiple used arguments", () => {
-                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
-                        parser._inputArguments(["abc", "--number", "-n", "15", "-s"]);
-                        const results = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
-
-                        expect(results.length).to.equal(2);
-
-                        expect(results[0].schema.name).to.equal("number");
-                        expect(results[0].identifierIndex).to.equal(1);
-                        expect(results[0].usedIdentifier).to.equal("--number");
-
-                        expect(results[1].schema.name).to.equal("string");
-                        expect(results[1].identifierIndex).to.equal(4);
-                        expect(results[1].usedIdentifier).to.equal("-s");
-                    });
-
-                    it("should only find the index for the first supplied identifier", () => {
-                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
-                        parser._inputArguments(["abc", "-n", "--number", "15"]);
-                        const results = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
-
-                        expect(results.length).to.equal(1);
-
-                        expect(results[0].schema.name).to.equal("number");
-                        expect(results[0].identifierIndex).to.equal(1);
-                        expect(results[0].usedIdentifier).to.equal("-n")
-                    });
-
-                    it("should find no index results if none of the supplied schemas were detected", () => {
-                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
-                        parser._inputArguments(["abc", "def"]);
-                        const results = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
-
-                        expect(results.length).to.equal(0);
-                    })
-                });
-
-                describe("Finding Value Index Results for explicit numberOfArguments", () => {
-                    it("should find index results for a value", () => {
-                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
-                        parser._inputArguments(["--number", "15"]);
-                        const identifierIndexResults = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArguments);
-                        const results = parser.getOptionalValueIndexResults(identifierIndexResults);
-
-                        expect(results.length).to.equal(1);
-                        expect(results[0].from).to.equal(1);
-                        expect(results[0].to).to.equal(2);
-                        expect(results[0].identifierIndexResult).to.deep.equal(identifierIndexResults[0]);
-                    })
-                });
-
-                describe("Finding Value Index Results for ambigous numberOfArguments", () => {
-                    const testArgumentBuilderOptionalNumberAmbigousBuilder = optionalArgument("number", ["--number", "-n"])
-                        .numberOfArguments("+")
-                        .number()
-                        .min(10)
-                        .max(20);
-                    const testArgumentBuilderOptionalNumberAmbigous = <OptionalArgumentSchema>testArgumentBuilderOptionalNumberAmbigousBuilder.build();
-
-                    const testArgumentBuilderOptionalStringAmbigousBuilder = optionalArgument("string", ["--string", "-s"])
-                        .numberOfArguments("?")
-                        .default("my string!")
-                        .string()
-                    const testArgumentBuilderOptionalStringAmbigous = <OptionalArgumentSchema>testArgumentBuilderOptionalStringAmbigousBuilder.build();
-
-                    const testOptionalArgumentsAmbigous: OptionalArgumentSchema[] = [testArgumentBuilderOptionalStringAmbigous, testArgumentBuilderOptionalNumberAmbigous];
-
-                    it("should find index results for a value", () => {
-                        const parser = new ArgumentParser(testRequiredArguments, testOptionalArgumentsAmbigous);
-                        parser._inputArguments(["--number", "15"]);
-                        const identifierIndexResults = parser.getOptionalSchemaIdentifierIndexResults(testOptionalArgumentsAmbigous);
-                        const results = parser.getOptionalValueIndexResults(identifierIndexResults);
-
-                        expect(results.length).to.equal(2);
-
-                        expect(results[0].from).to.equal(1);
-                        expect(results[0].to).to.equal(2);
-                        expect(results[0].identifierIndexResult).to.deep.equal(identifierIndexResults[0]);
-
-                        expect(results[1].from).to.equal(1);
-                        expect(results[1].to).to.equal(2);
-                        expect(results[1].identifierIndexResult.schema.name).to.equal(testArgumentBuilderOptionalStringAmbigous.name);
-                    })
-                });
-            });
+            expect(() => parser.getOptionalForIdentifier("not an identifier")).to.throw();
         })
     });
+
+    describe("Parse Arguments", () => {
+        it("should parse arguments", () => {
+            const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
+            const result = parser.parse(["xyz", "--number", "1"]);
+        });
+
+        it("should throw an error if all required arguments are filled but there is still non identifier tokens remaining", () => {
+            const parser = new ArgumentParser(testRequiredArguments, testOptionalArguments);
+
+            expect(() => parser.parse(["valid_token", "invalid_token"]))
+        })
+    })
 });
