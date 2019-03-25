6379b5c39bf15399b9d21002e168be08a311c157
rib_yaboi <iloveproxies@protonmail.com>
2019-03-25 18:37:29 +0100

    removed old validation types and classes

--- src/dcommander/validation/argumentValue/valueRule/validation.value.rules.ts
+++
@@ -1,8 +0,0 @@
-export interface ValueRule {
-    readonly ruleFunction: RuleFunction;
-    readonly errorFormatter: ErrorFormatter
-}
-
-export type RuleFunction = (value: any) => boolean;
-export type ErrorFormatter = ((value: string) => string) | string;
-
