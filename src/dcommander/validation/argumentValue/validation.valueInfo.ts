e4ac34c097af43cb5b07cbf0c7dd7c9f510f816b
rib_yaboi <iloveproxies@protonmail.com>
2019-03-25 20:05:08 +0100

    removed unnecessary valueinfo file

--- src/dcommander/validation/argumentValue/validation.valueInfo.ts
+++
@@ -1,7 +0,0 @@
-import {ValueRule} from "./valueRule/validation.value.rules";
-import {ValueType} from "./valueType/validation.value.type";
-
-export interface ValueValidationInfo {
-    valueType: ValueType,
-    valueRules: ValueRule[]
-}
