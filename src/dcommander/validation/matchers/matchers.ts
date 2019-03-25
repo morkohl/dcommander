e9e726acf552f4580d826803009eae48863f9f31
rib_yaboi <iloveproxies@protonmail.com>
2019-03-25 16:27:30 +0100

    moved matchers to their own module so that they can be tested, added matcher test

---
+++ src/dcommander/validation/matchers/matchers.ts
@@ -0,0 +1,130 @@
+import {Constants} from "./constants";
+import {Errors} from "../../error/errors";
+
+export interface Matcher {
+    isMatching(value: any): boolean,
+    validationErrorMessage: Errors.ValidationErrorMessageFormatter
+}
+
+export namespace Matchers {
+
+    export function fulfillsCondition(condition: (value: any) => boolean, validationErrorMessage?: Errors.ValidationErrorMessageFormatter): Matcher {
+        return {
+            isMatching(value: any): boolean {
+                return condition(value);
+            },
+            validationErrorMessage: validationErrorMessage ? validationErrorMessage : value => `${value} does not fulfill the condition`
+        }
+    }
+
+    export namespace NumberMatchers {
+        export function inRange(from: number, to: number): Matcher {
+            return {
+                isMatching(value: number): boolean {
+                    return value >= from && value <= to;
+                },
+                validationErrorMessage: value => `${value} is not in range of ${from}, ${to}`
+            }
+        }
+
+        export function equalsOrBelowMaximum(maximum: number): Matcher {
+            return {
+                isMatching(value: number): boolean {
+                    return value <= maximum
+                },
+                validationErrorMessage: value => `${value} is above maximum value ${maximum}`
+            }
+        }
+
+        export function equalsOrAboveMinimum(minimum: number): Matcher {
+            return {
+                isMatching(value: number): boolean {
+                    return value >= minimum;
+                },
+                validationErrorMessage: value => `${value} is below minimum value ${minimum}`
+            }
+        }
+    }
+
+    export namespace StringMatchers {
+        export function isRegex(regex: RegExp, validationErrorMessage?: Errors.ValidationErrorMessageFormatter): Matcher {
+            return {
+                isMatching(value: string): boolean {
+                    return regex.test(value);
+                },
+                validationErrorMessage: validationErrorMessage ? validationErrorMessage : value => `${value} does not fulfill regex ${regex.toString()}`
+
+            };
+        }
+
+        export function isEmail(): Matcher {
+            return isRegex(Constants.String.Regexps.emailRegex, value => `${value} is not a valid E-mail`)
+        }
+
+        export function isIPAddress(ipVersion: Constants.String.IPProtocolVersion): Matcher {
+            if(ipVersion === Constants.String.IPProtocolVersion.IPV4) {
+                return isRegex(Constants.String.Regexps.ipv4Regex);
+            } else if(ipVersion === Constants.String.IPProtocolVersion.IPV6) {
+                return isRegex(Constants.String.Regexps.ipv6Regex);
+            } else {
+                throw new Error(`${ipVersion} is not a valid ip protocol version`);
+            }
+        }
+
+        export function isURL(): Matcher {
+            return isRegex(Constants.String.Regexps.urlRegex, value => `${value} is not a valid URL`)
+        }
+    }
+
+    export namespace BooleanMatchers {
+        export function isTrue(): Matcher {
+            return {
+                isMatching(value: any): boolean {
+                    return value;
+                },
+                validationErrorMessage: value => `${value} is not true`
+            }
+        }
+
+        export function isFalse(): Matcher {
+            return {
+                isMatching(value: boolean): boolean {
+                    return !value
+                },
+                validationErrorMessage: value => `${value} is not false`
+            }
+        }
+    }
+
+    export namespace DateMatchers {
+
+        export function isBefore(latest: Date): Matcher {
+            return {
+                isMatching(value: Date): boolean {
+                    return latest.getTime() > value.getTime();
+                },
+                validationErrorMessage: (value: Date) => `${value.toUTCString()} is not before ${latest.toUTCString()}`
+            }
+        }
+
+        export function isAfter(earliest: Date): Matcher {
+            return {
+                isMatching(value: Date): boolean {
+                    return earliest.getTime() < value.getTime();
+                },
+                validationErrorMessage: (value: Date) => `${value.toUTCString()} is not after ${earliest.toUTCString()}`
+            }
+        }
+    }
+
+    export namespace ObjectMatchers {
+        export function hasOwnProperty(propertyName: string): Matcher {
+            return {
+                isMatching(object: any): boolean {
+                    return object.hasOwnProperty(propertyName);
+                },
+                validationErrorMessage: value => `${value} does not have own property ${propertyName}`
+            }
+        }
+    }
+}
---
+++ test/matcher.test.ts
@@ -0,0 +1,174 @@
+import * as chai from 'chai';
+import {Matchers} from "../src/dcommander/validation/matchers/matchers";
+import {Constants} from "../src/dcommander/validation/matchers/constants";
+
+const expect = chai.expect;
+
+describe("Matcher Test", () => {
+    describe("fulfillsCondition", () => {
+        const matcher = Matchers.fulfillsCondition((value: any) => value === 1);
+
+        it("should return true if the supplied value is valid", () => {
+            expect(matcher.isMatching(1)).to.be.true;
+        });
+        it("should return false if the supplied value is invalid", () => {
+            expect(matcher.isMatching(2)).to.be.false;
+        });
+    });
+
+    describe("NumberMatchers Test", () => {
+        describe("inRange", () => {
+            const matcher = Matchers.NumberMatchers.inRange(0, 10);
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcher.isMatching(0)).to.be.true;
+                expect(matcher.isMatching(5)).to.be.true;
+                expect(matcher.isMatching(10)).to.be.true;
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                expect(matcher.isMatching(-1)).to.be.false;
+                expect(matcher.isMatching(11)).to.be.false;
+            });
+        });
+        describe("equalsOrAboveMinimum", () => {
+            const matcher = Matchers.NumberMatchers.equalsOrAboveMinimum(10);
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcher.isMatching(10)).to.be.true;
+                expect(matcher.isMatching(11)).to.be.true;
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                expect(matcher.isMatching(9)).to.be.false;
+                expect(matcher.isMatching(-1)).to.be.false;
+            });
+        });
+        describe("equalsOrBelowMaximum", () => {
+            const matcher = Matchers.NumberMatchers.equalsOrBelowMaximum(10);
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcher.isMatching(10)).to.be.true;
+                expect(matcher.isMatching(9)).to.be.true;
+                expect(matcher.isMatching(-1)).to.be.true;
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                expect(matcher.isMatching(11)).to.be.false;
+                expect(matcher.isMatching(200)).to.be.false;
+
+            });
+        });
+    });
+    describe("StringMatchers Test", () => {
+        describe("isRegex", () => {
+            const matcher = Matchers.StringMatchers.isRegex(/^[a-z]+$/i);
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcher.isMatching("h")).to.be.true;
+                expect(matcher.isMatching("hello")).to.be.true;
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                expect(matcher.isMatching("Hello world")).to.be.false;
+                expect(matcher.isMatching("1234")).to.be.false;
+            });
+        });
+        describe("isEmail", () => {
+            const matcher = Matchers.StringMatchers.isEmail();
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcher.isMatching("john-doe@mail.com")).to.be.true;
+                expect(matcher.isMatching("john.doe@mail-domain.com")).to.be.true
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                expect(matcher.isMatching("john.doe")).to.be.false
+
+            });
+        });
+        describe("isIPAddress", () => {
+            const matcherIPv4 = Matchers.StringMatchers.isIPAddress(Constants.String.IPProtocolVersion.IPV4);
+            const matcherIPv6 = Matchers.StringMatchers.isIPAddress(Constants.String.IPProtocolVersion.IPV6);
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcherIPv4.isMatching("192.168.172.0")).to.be.true;
+                expect(matcherIPv4.isMatching("0.0.0.0")).to.be.true;
+                expect(matcherIPv4.isMatching("10.10.255.10")).to.be.true;
+
+                expect(matcherIPv6.isMatching("::1")).to.be.true;
+                expect(matcherIPv6.isMatching("::")).to.be.true;
+                expect(matcherIPv6.isMatching("01::")).to.be.true;
+                expect(matcherIPv6.isMatching("fe80::")).to.be.true;
+                expect(matcherIPv6.isMatching("0000:0000:0001:1::")).to.be.true;
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                expect(matcherIPv4.isMatching("192.168.0.")).to.be.false;
+                expect(matcherIPv4.isMatching("not an ipv4 address")).to.be.false;
+                expect(matcherIPv4.isMatching("10.8.0.256")).to.be.false;
+
+                expect(matcherIPv6.isMatching("0000:0000::0001:1::")).to.be.false;
+                expect(matcherIPv6.isMatching(":::")).to.be.false;
+                expect(matcherIPv6.isMatching("0000:0000:0000:0000:0000:0000:0000:0000:0001")).to.be.false;
+
+            });
+        });
+        describe("isURL", () => {
+            const matcher = Matchers.StringMatchers.isURL();
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcher.isMatching("https://google.com")).to.be.true;
+                expect(matcher.isMatching("http://subdomain.domain.com/path?q=10&p=20")).to.be.true;
+                expect(matcher.isMatching("domain.com")).to.be.true;
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                //sockets are not supported yet
+                expect(matcher.isMatching("ws+unix:///var/run/docker.sock")).to.be.false;
+                expect(matcher.isMatching("http:/google.com")).to.be.false;
+                expect(matcher.isMatching("htptps://google.com/path/to/something")).to.be.false;
+
+            });
+        });
+    });
+    describe("BooleanMatchers Test", () => {
+        describe("isTrue", () => {
+            const matcher = Matchers.BooleanMatchers.isTrue();
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcher.isMatching(true)).to.be.true;
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                expect(matcher.isMatching(false)).to.be.false;
+
+            });
+        });
+        describe("isFalse", () => {
+            const matcher = Matchers.BooleanMatchers.isFalse();
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcher.isMatching(false)).to.be.true;
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                expect(matcher.isMatching(true)).to.be.false;
+            });
+        });
+    });
+    describe("ObjectMatchers Test", () => {
+        describe("hasOwnProperty", () => {
+            const matcher = Matchers.ObjectMatchers.hasOwnProperty("property");
+
+            it("should return true if the supplied value is valid", () => {
+                expect(matcher.isMatching({property:"string"})).to.be.true;
+            });
+
+            it("should return false if the supplied value is invalid", () => {
+                expect(matcher.isMatching({string:"property"})).to.be.false
+
+            });
+        });
+    });
+});
