20c8b5cafcd64ac521f8f00cf7d84324bd1e2f8a
rib_yaboi <iloveproxies@protonmail.com>
2019-03-25 16:28:52 +0100

    moved types to their own package, added datetype, added datetype test

---
+++ src/dcommander/argument/valueType/types.ts
@@ -0,0 +1,133 @@
+import {Errors} from "../../error/errors";
+
+export namespace Types {
+
+    import ConversionError = Errors.ConversionError;
+
+    export interface ValueType {
+        is(value: string): boolean;
+
+        convertValue(value: string): any;
+
+        are(values: string[]): boolean;
+
+        convertValues(values: string[]): any[];
+    }
+
+    abstract class ValueTypeImpl implements ValueType {
+        abstract convertValue(value: string): any;
+
+        convertValues(values: string[]): any {
+            return values.map(this.convertValue)
+        }
+
+        is(value: string): boolean {
+            try {
+                this.convertValue(value);
+                return true;
+            } catch (error) {
+                return false;
+            }
+        }
+
+        are(values: string[]): boolean {
+            return !!values.length ? true : this.is(values[0]) && this.are(values.splice(1, values.length));
+        }
+    }
+
+    export class StringValueType extends ValueTypeImpl {
+        is(value: string): boolean {
+            return true;
+        }
+
+        convertValue(value: string): string {
+            return value;
+        }
+
+        convertValues(values: string[]): string[] {
+            return super.convertValues(values) as string[];
+        }
+    }
+
+    export class NumberValueType extends ValueTypeImpl {
+        is(value: string): boolean {
+            return !isNaN(Number(value));
+        }
+
+        convertValue(value: string): number | never{
+            const castResult = Number(value);
+            if (isNaN(castResult)) {
+                throw new Errors.ConversionError(`could not convert ${value} to a number`);
+            }
+            return castResult;
+        }
+
+        convertValues(values: string[]): number[] | never {
+            return super.convertValues(values) as number[];
+        }
+    }
+
+    export class BooleanValueType extends ValueTypeImpl {
+        convertValue(value: string): boolean | never {
+            if (BooleanValueType.equalsFalse(value)) {
+                return false;
+            }
+            if (BooleanValueType.equalsTrue(value)) {
+                return true;
+            }
+            throw new Errors.ConversionError(`could not convert ${value} to a boolean`);
+        }
+
+        convertValues(values: string[]): boolean[] | never {
+            return super.convertValues(values) as boolean[];
+        }
+
+        static equalsFalse(value: string): boolean {
+            return /(0|n|no|false)/ig.test(value);
+        }
+
+        static equalsTrue(value: string): boolean {
+            return /(1|y|yes|true)/ig.test(value)
+        }
+    }
+
+    export class ObjectValueType<S> extends ValueTypeImpl {
+        readonly typeName: string;
+
+        constructor(typeName?: string) {
+            super();
+            this.typeName = typeName ? typeName : "object";
+        }
+
+        convertValue(value: string): S | any | never {
+            if(!value.startsWith("{") || !value.endsWith("}")) {
+                this.throwError(value);
+            }
+
+            try {
+                return JSON.parse(value) as S
+            } catch(error) {
+                this.throwError(value, error);
+            }
+        }
+
+        private throwError(value: string, additionalError?: Error): ConversionError {
+            throw new ConversionError(`could not convert ${value} to ${this.typeName}`, additionalError);
+        }
+
+        convertValues(values: string[]): S[] | any | never{
+            return super.convertValues(values) as S[];
+        }
+    }
+
+    export class DateValueType extends ValueTypeImpl {
+        convertValue(value: string): Date | never {
+            let date = new Date(value);
+            let dateFromValueAsNumber = new Date(Number(value));
+            if(!isNaN(date.getTime()) || !isNaN(dateFromValueAsNumber.getTime())) {
+                return !isNaN(date.getTime()) ? date : dateFromValueAsNumber
+            }
+            throw new Errors.ConversionError(`${value} is an invalid date`);
+        }
+    }
+}
--- test/type.test.ts
+++ test/type.test.ts
@@ -1,8 +1,12 @@
 import * as chai from "chai";
-import {NumberValueType} from "../src/dcommander/validation/argumentValue/valueType/number.value.type";
-import {StringValueType} from "../src/dcommander/validation/argumentValue/valueType/string.value.type";
-import {ObjectValueType} from "../src/dcommander/validation/argumentValue/valueType/object.value.type";
-import {BooleanValueType} from "../src/dcommander/validation/argumentValue/valueType/boolean.value.type";
+import {Types} from "../src/dcommander/argument/valueType/types";
+import NumberValueType = Types.NumberValueType;
+import StringValueType = Types.StringValueType;
+import BooleanValueType = Types.BooleanValueType;
+import ObjectValueType = Types.ObjectValueType;
+import DateValueType = Types.DateValueType;
+import {Errors} from "./dcommander/error/errors";
+import ConversionError = Errors.ConversionError;
 
 const expect = chai.expect;
 
@@ -10,17 +14,21 @@ describe("ValueType Test", () => {
     describe("NumberValueType Test", () => {
         it("should be true if a value is a valid number", () => {
             const type = new NumberValueType();
+
             expect(type.is("1")).to.be.true;
+            expect(type.is("0x12")).to.be.true;
+
             expect(type.is("19a")).to.be.false;
             expect(type.is("abc")).to.be.false;
-            expect(type.is("0x12")).to.be.true;
         });
 
         it("should convert to a valid number", () => {
             const type = new NumberValueType();
+
             expect(type.convertValue("1")).to.eq(1);
             expect(type.convertValue("1234")).to.eq(1234);
             expect(type.convertValue("0x12")).to.eq(18);
+
             expect(() => type.convertValue("a")).to.throw();
         });
     });
@@ -28,12 +36,14 @@ describe("ValueType Test", () => {
     describe("StringValueType Test", () => {
         it("should be true if a value is a valid string", () => {
             const type = new StringValueType();
+
             expect(type.is("discord is spying on you right now")).to.be.true;
             expect(type.is("1")).to.be.true;
         });
 
         it("should convert to a valid string", () => {
             const type = new StringValueType();
+
             expect(type.convertValue("discord is spying on you right now")).to.eq("discord is spying on you right now");
         });
     });
@@ -41,6 +51,7 @@ describe("ValueType Test", () => {
     describe("BooleanValueType Test", () => {
         it("should be true if a value is a valid symbol that could point to a boolean expression", () => {
             const type = new BooleanValueType();
+
             expect(type.is("true")).to.be.true;
             expect(type.is("1")).to.be.true;
             expect(type.is("y")).to.be.true;
@@ -49,14 +60,18 @@ describe("ValueType Test", () => {
             expect(type.is("0")).to.be.true;
             expect(type.is("n")).to.be.true;
             expect(type.is("no")).to.be.true;
+
+            expect(type.is("anything else")).to.be.false;
+            expect(type.is("2")).to.be.false;
         });
 
-        it("should convert any value to the same as before", () => {
+        it("should convert to a valid boolean", () => {
             const type = new BooleanValueType();
             expect(type.convertValue("true")).to.be.true;
             expect(type.convertValue("1")).to.be.true;
             expect(type.convertValue("y")).to.be.true;
             expect(type.convertValue("yes")).to.be.true;
+
             expect(type.convertValue("false")).to.be.false;
             expect(type.convertValue("0")).to.be.false;
             expect(type.convertValue("n")).to.be.false;
@@ -69,8 +84,9 @@ describe("ValueType Test", () => {
             const type = new ObjectValueType();
             expect(type.is("{}")).to.be.true;
             expect(type.is("{ \"a\": \"1\", \"b\": \"2\" }")).to.be.true;
-            expect(type.is("[1, 2, 3]")).to.be.true;
-            expect(type.is("1")).to.be.true;
+
+            expect(type.is("[1, 2, 3]")).to.be.false;
+            expect(type.is("1")).to.be.false;
             expect(type.is("a string")).to.be.false;
         });
 
@@ -78,9 +94,29 @@ describe("ValueType Test", () => {
             const type = new ObjectValueType();
             expect(type.convertValue("{ \"foo\": true, \"bar\": 1 }")).to.deep.eq({ foo: true, bar: 1});
             expect(type.convertValue("{}")).to.deep.eq({});
-            expect(type.convertValue("[1, 2, 3]")).to.deep.eq([1, 2, 3]);
-            expect(type.convertValue("1")).to.eq(1);
+
+            expect(() => type.convertValue("1")).to.throw();
+            expect(() => type.convertValue("[1,2,3]")).to.throw();
             expect(() => type.convertValue("a string")).to.throw();
         })
     });
+
+    describe("DateValueType Test", () => {
+        it("should be true for a valid date", () => {
+            const type = new DateValueType();
+            expect(type.is("Mon, 25 Mar 2019 14:46:45 GMT")).to.be.true;
+            expect(type.is("1553525305044")).to.be.true;
+            expect(type.is("Mon Mar 25 2019")).to.be.true;
+        });
+
+        it("should convert any valid JSON to an object", () => {
+            const type = new DateValueType();
+            expect(type.convertValue("Mon, 25 Mar 2019 14:46:45 GMT")).to.deep.eq(new Date("Mon, 25 Mar 2019 14:46:45 GMT"));
+            expect(type.convertValue("1553525305044")).to.deep.eq(new Date(1553525305044));
+            expect(type.convertValue("Mon Mar 25 2019")).to.deep.eq(new Date("Mon Mar 25 2019"));
+
+            expect(() => type.convertValue("a string")).to.throw();
+        })
+    });
+
 });
