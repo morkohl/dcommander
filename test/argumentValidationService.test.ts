32bda36c33516e85b4bb3911aec9c3e6f70d0f74
rib_yaboi <iloveproxies@protonmail.com>
2019-04-12 18:08:13 +0200

    validation test

---
+++ test/argumentValidationService.test.ts
@@ -0,0 +1,79 @@
+import * as chai from 'chai';
+import {ArgumentBuilders} from "../src/dcommander/builder/argument/argumentBuilders";
+import optionalArgumentSchema = ArgumentBuilders.optionalArgumentSchema;
+import {ArgumentValidation} from "../src/dcommander/validation/validationService";
+
+
+const expect = chai.expect;
+
+const testArgumentName = "test";
+
+
+describe("ArgumentValidationService Test", () => {
+    it("should return a validation result without errors if the arguments passed were valid", () => {
+        const testParsedArgument = {
+            schema: optionalArgumentSchema(testArgumentName).number(
+                numberBuilder => numberBuilder.range(1,5)
+            ).build(),
+            values: [1,2,3],
+            excludeValidation: false
+        };
+
+        const validationResult = ArgumentValidation.validate([testParsedArgument]);
+
+        expect(validationResult.hasErrors()).to.be.false;
+        expect(validationResult.errors.length).to.eq(0);
+        expect(validationResult.getMessage()).to.eq("");
+    });
+
+    it("should return a validation result with errors", () => {
+        const testParsedArgument = {
+            schema: optionalArgumentSchema(testArgumentName).number(
+                numberBuilder => numberBuilder.range(0,2)
+            ).build(),
+            values: [3,4],
+            excludeValidation: false
+        };
+
+        const validationResult = ArgumentValidation.validate([testParsedArgument]);
+
+        expect(validationResult.hasErrors()).to.be.true;
+        expect(validationResult.errors.length).to.eq(1);
+        expect(validationResult.getMessage()).to.eq("3 is not in range of 0, 2")
+    });
+
+    it("should catch all errors if that option was passed", () => {
+        const testParsedArgument = {
+            schema: optionalArgumentSchema(testArgumentName).number(
+                numberBuilder => numberBuilder.range(0,2)
+            ).build(),
+            values: [3,4],
+            excludeValidation: false
+        };
+
+        const validationResult = ArgumentValidation.validate([testParsedArgument], {
+            gatherAllValidationErrors: true,
+            errorFormatSeparator: "; "
+        });
+
+        expect(validationResult.hasErrors()).to.be.true;
+        expect(validationResult.errors.length).to.eq(2);
+        expect(validationResult.getMessage()).to.eq("3 is not in range of 0, 2; 4 is not in range of 0, 2");
+    });
+
+    it("should exclude parsed arguments from validation where the corresponding flag was set", () => {
+        const testParsedArgument = {
+            schema: optionalArgumentSchema(testArgumentName).number(
+                numberBuilder => numberBuilder.range(0,2)
+            ).build(),
+            values: [3,4],
+            excludeValidation: true
+        };
+
+        const validationResult = ArgumentValidation.validate([testParsedArgument]);
+
+        expect(validationResult.hasErrors()).to.be.false;
+        expect(validationResult.errors.length).to.eq(0);
+        expect(validationResult.getMessage()).to.eq("");
+    })
+});
