9217d77396fbee4c1c188d7d542fa1af4b3fd91f
rib_yaboi <iloveproxies@protonmail.com>
2019-05-05 17:44:04 +0200

    include sanitization to argumentschema

--- src/dcommander/argument/argumentSchema.ts
+++ src/dcommander/argument/argumentSchema.ts
@@ -7,6 +7,7 @@ export interface ArgumentSchema {
     readonly argumentInfo: ArgumentInfo;
     readonly valueInfo: ValueInfo;
     readonly validationMatchers: Matcher[];
+    readonly sanitization?: SanitizationFunction;
 }
 
 export interface OptionalArgumentSchema extends ArgumentSchema {
@@ -21,14 +22,16 @@ interface ArgumentInfo {
     readonly usage?: string;
 }
 
-export enum AMBIGUITIES {
-    ALL_OR_DEFAULT = '?',
-    AT_LEAST_ONE = '+'
-}
-export type ArgumentsLength = AMBIGUITIES | number;
+export type SanitizationFunction = (value: any) => any;
 
 export interface ValueInfo {
     readonly valueType: ValueType
     readonly argumentsLength: ArgumentsLength;
     readonly defaultValue?: any
 }
+export enum AMBIGUITIES {
+    ALL_OR_DEFAULT = '?',
+    AT_LEAST_ONE = '+'
+}
+export type ArgumentsLength = AMBIGUITIES | number;
+
